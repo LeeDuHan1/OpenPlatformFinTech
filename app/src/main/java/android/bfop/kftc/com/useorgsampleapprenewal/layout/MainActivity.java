091d1e48bfcb5ccb8238cad8ce9904c9d6706aa0
Bruce <itpsolver@gmail.com>
2017-06-13 19:28:52 +0900

    작업중

--- app/src/main/java/android/bfop/kftc/com/useorgsampleapprenewal/layout/MainActivity.java
+++ app/src/main/java/android/bfop/kftc/com/useorgsampleapprenewal/layout/MainActivity.java
@@ -83,23 +83,26 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
         return super.onOptionsItemSelected(item);
     }
 
+    /**
+     * 네비게이션메뉴 선택 이벤트핸들러
+     *
+     * @param item
+     * @return
+     */
     @SuppressWarnings("StatementWithEmptyBody")
     @Override
     public boolean onNavigationItemSelected(MenuItem item) {
-        // Handle navigation view item clicks here.
+
         int id = item.getItemId();
+        if (id == R.id.nav_authNew) {
 
-        if (id == R.id.nav_camera) {
-            // Handle the camera action
-        } else if (id == R.id.nav_gallery) {
+        } else if (id == R.id.nav_authOldApp) {
 
-        } else if (id == R.id.nav_slideshow) {
+        } else if (id == R.id.nav_authOldWeb) {
 
-        } else if (id == R.id.nav_manage) {
+        } else if (id == R.id.nav_APICall) {
 
-        } else if (id == R.id.nav_share) {
-
-        } else if (id == R.id.nav_send) {
+        } else if (id == R.id.nav_setting) {
 
         }
 
