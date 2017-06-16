349bc1ebd97c09677275035df28dbdb232056225
Bruce <itpsolver@gmail.com>
2017-06-16 09:36:50 +0900

    작업중

--- app/src/main/java/android/bfop/kftc/com/useorgsampleapprenewal/layout/SettingsFragment.java
+++ app/src/main/java/android/bfop/kftc/com/useorgsampleapprenewal/layout/SettingsFragment.java
@@ -3,14 +3,21 @@ package android.bfop.kftc.com.useorgsampleapprenewal.layout;
 import android.bfop.kftc.com.useorgsampleapprenewal.App;
 import android.bfop.kftc.com.useorgsampleapprenewal.R;
 import android.bfop.kftc.com.useorgsampleapprenewal.util.Constants;
+import android.bfop.kftc.com.useorgsampleapprenewal.util.MessageUtil;
 import android.bfop.kftc.com.useorgsampleapprenewal.util.StringUtil;
 import android.content.Context;
+import android.content.DialogInterface;
+import android.content.SharedPreferences;
 import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.webkit.CookieManager;
+import android.webkit.ValueCallback;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
@@ -117,6 +124,9 @@ public class SettingsFragment extends Fragment implements Button.OnClickListener
      */
     public void bindButtonClickEvents(View view){
 
+        ((Button)view.findViewById(R.id.radioSvr_TEST)).setOnClickListener(this); // 테스트서버 선택 라디오버튼
+        ((Button)view.findViewById(R.id.radioSvr_PRD)).setOnClickListener(this); // 운영서버 선택 라디오버튼
+
         ((Button)view.findViewById(R.id.btnSaveSettings)).setOnClickListener(this); // 저장 버튼
         ((Button)view.findViewById(R.id.btnResetSettings)).setOnClickListener(this); // 초기화 버튼
         ((Button)view.findViewById(R.id.btnRemoveSession)).setOnClickListener(this); // 세션쿠키제거 버튼
@@ -131,15 +141,125 @@ public class SettingsFragment extends Fragment implements Button.OnClickListener
     public void onClick(View v) {
 
         switch(v.getId()){
-            case R.id.btnAuthOldWeb:
+
+            case R.id.radioSvr_TEST:
+            case R.id.radioSvr_PRD:
+                String env = getEnvFromRadioButton(); // 클릭된 호출서버 라디오버튼에서 선택한 환경값 획득
+                Log.d("!@#", "env: "+env);
+                loadPreferences(env);
                 break;
-            case R.id.btnRegAcntOldWeb:
+
+            case R.id.btnSaveSettings:
+                save();
                 break;
-            case R.id.btnAuthAcntOldWeb:
+
+            case R.id.btnResetSettings:
+                reset();
                 break;
+
+            case R.id.btnRemoveSession:
+                removeSessionCookie();
+                break;
+
             default:
                 break;
         }
+    }
+
+    /**
+     * 사용자 로그인 세션이 계속 남아있어서, 제거를 위해 추가한 메서드
+     */
+    public void removeSessionCookie() {
+
+        CookieManager cookieManager = CookieManager.getInstance();
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            cookieManager.removeSessionCookies(new ValueCallback<Boolean>() {
+                @Override
+                public void onReceiveValue(Boolean value) {
+                    Log.d("", "## 롤리팝 이상 버전의 removeSessionCookie() 호출 후");
+                }
+            });
+
+        }else{
+            cookieManager.removeSessionCookie();
+            Log.d("", "## 롤리팝 미만 버전의 removeSessionCookie() 호출 후");
+        }
+        MessageUtil.showToast("세션정보가 삭제되었습니다.", 1500);
+    }
+
+    /**
+     * 설정값을 SharedPreferences 에 저장
+     */
+    public void save() {
+
+        SharedPreferences.Editor editor = App.getPref().edit();
+
+        // 클릭된 호출서버 라디오버튼에서 선택한 환경값 획득
+        String env = getEnvFromRadioButton();
+        editor.putString("ENV", env);
+
+        String es = App.getEnvSuffix(env);
+        editor.putString("APP_KEY" + es, m_etAppKey.getText().toString());
+        editor.putString("APP_SECRET" + es, m_etAppSecret.getText().toString());
+        editor.putString("WEB_CALLBACK_URL" + es, m_etWebCallbackUrl.getText().toString());
+        editor.putString("APP_CALLBACK_URL" + es, m_etCallbackUrl.getText().toString());
+        editor.putString("SCOPE" + es, m_spScope.getSelectedItem().toString());
+
+        editor.apply();
+
+        MessageUtil.showToast("저장되었습니다.", 1500);
+    }
+
+    /**
+     * 현재 SharedPreferences 에 저장되어 있는 설정값들을, Constants의 기본값으로 overwrite 한다.
+     */
+    public void reset(){
+
+        final String env = getEnvFromRadioButton();
+        String envName = App.getEnvName(env);
+
+        // Dialog를 사용하여 confirm창 처럼 활용한다.
+        MessageUtil.getDialogBuilder("", envName + " 호출용 설정을 초기화 하시겠습니까?", true, App.getAppContext())
+                .setPositiveButton("초기화", new DialogInterface.OnClickListener() {
+
+                    // 초기화 선택시
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+
+                        String es = App.getEnvSuffix(env);
+                        SharedPreferences.Editor editor = App.getPref().edit();
+
+                        editor.putString("ENV", env);
+                        editor.putString("APP_KEY" + es, StringUtil.getConstantsVal("APP_KEY" + es));
+                        editor.putString("APP_SECRET" + es, StringUtil.getConstantsVal("APP_SECRET" + es));
+                        editor.putString("WEB_CALLBACK_URL" + es, StringUtil.getConstantsVal("WEB_CALLBACK_URL" + es));
+                        editor.putString("APP_CALLBACK_URL" + es, StringUtil.getConstantsVal("APP_CALLBACK_URL" + es));
+                        editor.putString("SCOPE" + es, StringUtil.getConstantsVal("SCOPE" + es));
+
+                        editor.apply();
+
+                        loadPreferences(env);
+
+                        MessageUtil.showToast("초기화되었습니다.", 1500);
+                    }
+                }).setNegativeButton("취소", new DialogInterface.OnClickListener() {
+            // 취소 선택시
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                // do nothing
+            }
+        }).create().show();
+    }
+
+    /**
+     * 현재 라디오버튼 선택상태의 호출서버 구분자를 리턴
+     *
+     * @return
+     */
+    private String getEnvFromRadioButton() {
+
+        String radioSvrId = getResources().getResourceEntryName(rg_svr.getCheckedRadioButtonId());
+        return radioSvrId.substring(radioSvrId.indexOf('_')+1);
     }
 
     @Override
