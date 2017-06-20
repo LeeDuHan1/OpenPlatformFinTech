5df6b04fa2eeb5fcbc54f319400864ca002ad431
Bruce <itpsolver@gmail.com>
2017-06-20 10:10:10 +0900

    작업중

--- app/src/main/java/android/bfop/kftc/com/useorgsampleapprenewal/layout/SettingsFragment.java
+++ app/src/main/java/android/bfop/kftc/com/useorgsampleapprenewal/layout/SettingsFragment.java
@@ -78,10 +78,7 @@ public class SettingsFragment extends BaseFragment {
         // 버튼 이벤트핸들러 바인딩
         bindButtonClickEvents(view);
 
-        /*
-         * Fragment 초기화 이벤트를 EventBus를 통해서 post
-         *      - 액션바 햄버거메뉴를 뒤로가기 화살표버튼으로 교체하기 위해서 수행
-         */
+        // Fragment 초기화 이벤트를 EventBus를 통해서 post (액션바 햄버거메뉴를 뒤로가기 화살표버튼으로 교체하기 위해서 수행)
         EventBus.getDefault().post(new FragmentInitializedEvent(this.getClass(), true));
 
         rg_svr = (RadioGroup) view.findViewById(R.id.rgSvr);
