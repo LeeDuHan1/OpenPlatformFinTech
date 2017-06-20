48af13785ef27a90be3e06532553faff0a138b63
Bruce <itpsolver@gmail.com>
2017-06-20 15:47:08 +0900

    작업중

--- app/src/main/java/android/bfop/kftc/com/useorgsampleapprenewal/layout/MainActivity.java
+++ app/src/main/java/android/bfop/kftc/com/useorgsampleapprenewal/layout/MainActivity.java
@@ -36,6 +36,7 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
 
     private boolean toolbarNavigationListenerRegistered = false;
 
+    ///////////////////////////////////// Activity Lifecycle Callbacks - start /////////////////////////////////////
     @Override
     protected void onCreate(Bundle savedInstanceState) {
 
@@ -83,6 +84,19 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
         });
 
     }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        EventBus.getDefault().register(this);   // EventBus 등록
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        EventBus.getDefault().unregister(this); // EventBus 해지
+    }
+    ///////////////////////////////////// Activity Lifecycle Callbacks - end ///////////////////////////////////////
 
     /**
      * 뒤로가기 버튼이 눌렸을 때 최초로 호출되는 메서드
@@ -250,20 +264,7 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
     @Subscribe(threadMode = ThreadMode.MAIN)
     public void onFragmentInitialized(FragmentInitEvent event){
 
-        Log.d("@@", "## onFragmentInitialized called!! : " + event);
         showBackArrowOnActionBar(event.isBackArrowOnActionBar());
-    }
-
-    @Override
-    protected void onStart() {
-        super.onStart();
-        EventBus.getDefault().register(this);
-    }
-
-    @Override
-    protected void onStop() {
-        super.onStop();
-        EventBus.getDefault().unregister(this);
     }
 
 }
