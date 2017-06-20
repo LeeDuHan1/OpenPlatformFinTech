1accb656def88aaa8f950103e5f189f91f015bd6
Bruce <itpsolver@gmail.com>
2017-06-20 10:04:57 +0900

    작업중

--- app/src/main/java/android/bfop/kftc/com/useorgsampleapprenewal/layout/SettingsFragment.java
+++ app/src/main/java/android/bfop/kftc/com/useorgsampleapprenewal/layout/SettingsFragment.java
@@ -9,10 +9,8 @@ import android.bfop.kftc.com.useorgsampleapprenewal.util.StringUtil;
 import android.content.Context;
 import android.content.DialogInterface;
 import android.content.SharedPreferences;
-import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
-import android.support.v4.app.Fragment;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -27,26 +25,11 @@ import android.widget.Spinner;
 
 import org.greenrobot.eventbus.EventBus;
 
-
 /**
- * A simple {@link Fragment} subclass.
- * Activities that contain this fragment must implement the
- * {@link SettingsFragment.OnFragmentInteractionListener} interface
- * to handle interaction events.
- * Use the {@link SettingsFragment#newInstance} factory method to
- * create an instance of this fragment.
+ * 설정 Fragment
+ *
  */
-//public class SettingsFragment extends Fragment implements Button.OnClickListener {
 public class SettingsFragment extends BaseFragment {
-
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
 
     private RadioGroup rg_svr;
     private EditText m_etAppKey;
@@ -55,20 +38,34 @@ public class SettingsFragment extends BaseFragment {
     private EditText m_etCallbackUrl;
     private Spinner m_spScope;
 
-    private OnFragmentInteractionListener mListener;
-
+    /**
+     * 생성자
+     *  - 매개변수가 있는 생성자를 사용할 수 없는 제약이 있다.
+     */
     public SettingsFragment() {
         // Required empty public constructor
     }
+
+    /**
+     * Fragment 생성 메서드
+     *
+     * @param actionBarTitle
+     * @return
+     */
+    public static SettingsFragment newInstance(String actionBarTitle) {
+
+        SettingsFragment fragment = new SettingsFragment();
+        Bundle args = new Bundle();
+        args.putString(Constants.ACTIONBAR_TITLE, actionBarTitle);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
 
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
     }
 
     @Override
@@ -103,8 +100,28 @@ public class SettingsFragment extends BaseFragment {
         return view;
     }
 
+    @Override
+    public void onAttach(Context context) {
+
+        super.onAttach(context);
+    }
+
+    @Override
+    public void onDetach() {
+
+        super.onDetach();
+    }
+
+    @Override
+    public void doBackBehavior() {
+
+        mainActivity.back();
+    }
+
     /**
      * 버튼 이벤트핸들러 바인딩
+     *
+     *  - BaseFragment가 Button.OnClickListener 를 implement 했기 때문에 this를 바인딩 함.
      *
      * @param view
      */
@@ -112,7 +129,6 @@ public class SettingsFragment extends BaseFragment {
 
         ((Button)view.findViewById(R.id.radioSvr_TEST)).setOnClickListener(this); // 테스트서버 선택 라디오버튼
         ((Button)view.findViewById(R.id.radioSvr_PRD)).setOnClickListener(this); // 운영서버 선택 라디오버튼
-
         ((Button)view.findViewById(R.id.btnSaveSettings)).setOnClickListener(this); // 저장 버튼
         ((Button)view.findViewById(R.id.btnResetSettings)).setOnClickListener(this); // 초기화 버튼
         ((Button)view.findViewById(R.id.btnRemoveSession)).setOnClickListener(this); // 세션쿠키제거 버튼
@@ -241,24 +257,6 @@ public class SettingsFragment extends BaseFragment {
         return radioSvrId.substring(radioSvrId.indexOf('_')+1);
     }
 
-    @Override
-    public void onAttach(Context context) {
-
-        super.onAttach(context);
-        if (context instanceof OnFragmentInteractionListener) {
-            mListener = (OnFragmentInteractionListener) context;
-        } else {
-            throw new RuntimeException(context.toString() + " must implement OnFragmentInteractionListener");
-        }
-    }
-
-    @Override
-    public void onDetach() {
-
-        super.onDetach();
-        mListener = null;
-    }
-
     /**
      * input form 에 SharedPreferences 값 로딩
      */
@@ -275,69 +273,17 @@ public class SettingsFragment extends BaseFragment {
         m_spScope.setSelection(StringUtil.getPropString("SCOPE" + es).equals("inquiry") ? 0 : 1);
     }
 
-    @Override
-    public void doBackBehavior() {
-
-        mainActivity.back();
-    }
-
     /**
      * 매개변수로 호출서버 구분자를 받아서 라디오버튼 클릭상태 반영
      */
     private void setRadioSvrCheckedFromPref(String env){
 
         int id = 0;
-//        String env = App.getEnv(); // 파라미터로 받는 걸로 변경
         switch(env){
             case Constants.ENV_TEST: id = R.id.radioSvr_TEST; break;
             case Constants.ENV_PRD: id = R.id.radioSvr_PRD; break;
         }
         rg_svr.check(id);
     }
-
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment MainFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static SettingsFragment newInstance(String actionBarTitle) {
-
-        SettingsFragment fragment = new SettingsFragment();
-        Bundle args = new Bundle();
-        args.putString(Constants.ACTIONBAR_TITLE, actionBarTitle);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
-    /**
-     * This interface must be implemented by activities that contain this
-     * fragment to allow an interaction in this fragment to be communicated
-     * to the activity and potentially other fragments contained in that
-     * activity.
-     * <p>
-     * See the Android Training lesson <a href=
-     * "http://developer.android.com/training/basics/fragments/communicating.html"
-     * >Communicating with Other Fragments</a> for more information.
-     *
-     * TODO: 얘 꼭 필요해??
-     *
-     */
-    public interface OnFragmentInteractionListener {
-        // TODO: Update argument type and name
-        void onFragmentInteractionSettings(Uri uri);
-    }
-
-    // TODO: Rename method, update argument and hook method into UI event
-    public void onButtonPressed(Uri uri) {
-        if (mListener != null) {
-            mListener.onFragmentInteractionSettings(uri);
-        }
-    }
-
 
 }
