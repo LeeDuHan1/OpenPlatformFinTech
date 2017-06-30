22b891bfd09cac810ad2ea3b007a9e476f2aea3a
Bruce <itpsolver@gmail.com>
2017-06-30 16:00:44 +0900

    뒤로가기 재정리 완료

---
+++ app/src/main/java/android/bfop/kftc/com/useorgsampleapprenewal/layout/AuthNewWebPageAuthorize2Fragment.java
@@ -0,0 +1,98 @@
+package android.bfop.kftc.com.useorgsampleapprenewal.layout;
+
+import android.bfop.kftc.com.useorgsampleapprenewal.R;
+import android.bfop.kftc.com.useorgsampleapprenewal.eventbus.FragmentInitEvent;
+import android.bfop.kftc.com.useorgsampleapprenewal.util.Constants;
+import android.bfop.kftc.com.useorgsampleapprenewal.util.FragmentUtil;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import org.greenrobot.eventbus.EventBus;
+
+
+/**
+ * 사용자인증 개선버전 Fragment
+ */
+public class AuthNewWebPageAuthorize2Fragment extends BaseFragment {
+
+    private static String URI = "/oauth/2.0/authorize2";
+
+    /**
+     * 생성자
+     *  - 매개변수가 있는 생성자를 사용할 수 없는 제약이 있다.
+     */
+    public AuthNewWebPageAuthorize2Fragment() {
+        // Required empty public constructor
+    }
+
+    /**
+     * Fragment 생성 메서드
+     *
+     * @param actionBarTitle
+     * @return
+     */
+    public static AuthNewWebPageAuthorize2Fragment newInstance(String actionBarTitle) {
+
+        AuthNewWebPageAuthorize2Fragment fragment = new AuthNewWebPageAuthorize2Fragment();
+        Bundle args = new Bundle();
+        args.putString(Constants.ACTIONBAR_TITLE, actionBarTitle);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    //===================================== Fragment Lifecycle Callbacks - start =====================================
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+
+        // Inflate the layout for this fragment
+        View view = inflater.inflate(R.layout.fragment_authnewweb_authorize2, container, false);
+        super.initBaseFragment(view); // BaseFragment 초기화 수행
+
+        // Fragment 초기화 이벤트를 EventBus를 통해서 post (액션바 햄버거메뉴와 뒤로가기 화살표버튼을 상호 교체하기 위해서 수행)
+        EventBus.getDefault().post(new FragmentInitEvent(this.getClass(), true));
+
+        // 버튼 이벤트핸들러 바인딩
+        bindButtonClickEvents(view);
+
+        return view;
+    }
+    //===================================== Fragment Lifecycle Callbacks - end =======================================
+
+    /**
+     * 버튼 이벤트핸들러 바인딩
+     *
+     * @param view
+     */
+    public void bindButtonClickEvents(View view){
+
+//        ((Button)view.findViewById(R.id.btnAuthNewMenu)).setOnClickListener(this);
+    }
+
+    /**
+     * 버튼 onclick 이벤트핸들러
+     *
+     * @param v
+     */
+    @Override
+    public void onClick(View v) {
+
+    }
+
+    /**
+     * 뒤로가기 버튼을 눌렀을 때의 동작
+     */
+    @Override
+    public void onBackPressedForFragment() {
+
+        FragmentUtil.replaceNewFragment(AuthNewWebMenuFragment.class);
+    }
+
+}
