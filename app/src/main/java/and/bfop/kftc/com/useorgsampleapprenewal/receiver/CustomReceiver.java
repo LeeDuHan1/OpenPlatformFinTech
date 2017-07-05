19aca64ef88917bf0ed7ba2eef1c71df0b5c289e
Bruce <itpsolver@gmail.com>
2017-07-05 13:09:04 +0900

    SMS 권한 작업 추가

--- app/src/main/java/and/bfop/kftc/com/useorgsampleapprenewal/receiver/CustomReceiver.java
+++ app/src/main/java/and/bfop/kftc/com/useorgsampleapprenewal/receiver/CustomReceiver.java
@@ -3,25 +3,27 @@ package and.bfop.kftc.com.useorgsampleapprenewal.receiver;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
-import android.util.Log;
 
 import and.bfop.kftc.com.useorgsampleapprenewal.util.MessageUtil;
 
 /**
- * SMS 수신 BroadcaseReceiver
+ * 사용자정의 BroadcaseReceiver
+ *
+ *  - SMS수신 처리 목적으로 생성
  *
  * Created by LeeHyeonJae on 2017-07-04.
  */
-public class SMSReceiver extends BroadcastReceiver {
+public class CustomReceiver extends BroadcastReceiver {
 
     @Override
     public void onReceive(Context context, Intent intent) {
 
-        Log.d("##", "SMSReceiver.onReceive() called!");
-        MessageUtil.showToast("SMSReceiver.onReceive() called!");
-
+        // SMS 수신일 경우
         if("android.provider.Telephony.SMS_RECEIVED".equals(intent.getAction())){
-            // sms 수신
+            MessageUtil.showToast("SMS 수신!! >, <");
+
+
+
         }
 
     }
